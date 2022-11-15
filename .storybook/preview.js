8d678c11abd53d09515efb23bdee22b185f9fe12
Igor Rezende <98923958+Sr-Igor@users.noreply.github.com>
2022-11-15 18:49:28 -0300

    Initial Home

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,10 +2,26 @@ import { ThemeProvider } from 'styled-components'
 import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
 
+export const parameters = {
+  backgrounds: {
+    default: 'won-dark',
+    values: [
+      {
+        name: 'won-dark',
+        value: theme.colors.mainBg
+      },
+      {
+        name: 'won-light',
+        value: theme.colors.white
+      }
+    ]
+  }
+}
+
 export const decorators = [
   (Story) => (
     <ThemeProvider theme={theme}>
-      <GlobalStyles />
+      <GlobalStyles removeBg />
       <Story />
     </ThemeProvider>
   )
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,6 @@
 import { createGlobalStyle, css } from 'styled-components'
 
-const GlobalStyles = createGlobalStyle`
+const GlobalStyles = createGlobalStyle<{ removeBg?: boolean }>`
   @font-face {
     font-family: 'Poppins';
     font-style: normal;
@@ -34,9 +34,14 @@ const GlobalStyles = createGlobalStyle`
     box-sizing: border-box;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+
+    &::before,
+    &::after {
+      box-sizing: inherit;
+    }
   }
 
-  ${({ theme }) => css`
+  ${({ theme, removeBg }) => css`
     html {
       font-size: 62.5%;
     }
@@ -44,6 +49,10 @@ const GlobalStyles = createGlobalStyle`
     body {
       font-family: ${theme.font.family};
       font-size: ${theme.font.sizes.medium};
+      ${!removeBg &&
+      css`
+        background-color: ${theme.colors.mainBg};
+      `}
     }
   `}
 `
