fc86baf361ce4c48161c0f4b7609058220868369
Igor Rezende <98923958+Sr-Igor@users.noreply.github.com>
2022-11-30 20:38:01 -0300

    Game Page

--- src/components/GameDetails/test.tsx
+++ src/components/GameDetails/test.tsx
@@ -4,7 +4,7 @@ import { renderWithTheme } from 'utils/tests/helpers'
 import GameDetails, { GameDetailsProps } from '.'
 
 const props: GameDetailsProps = {
-  developer: 'Rockstar Games',
+  developer: 'Different Tales',
   releaseDate: '2020-11-21T23:00:00',
   platforms: ['windows', 'linux', 'mac'],
   publisher: 'Rockstar Games',
@@ -55,6 +55,18 @@ describe('<GameDetails />', () => {
     renderWithTheme(<GameDetails {...props} />)
 
     expect(screen.getByText(/free/i)).toBeInTheDocument()
+  })
+
+  it('should render the developer', () => {
+    renderWithTheme(<GameDetails {...props} />)
+
+    expect(screen.getByText(/Different Tales/i)).toBeInTheDocument()
+  })
+
+  it('should render the publisher', () => {
+    renderWithTheme(<GameDetails {...props} />)
+
+    expect(screen.getByText(/Rockstar Games/i)).toBeInTheDocument()
   })
 
   it('should render 18+ rating when BR18', () => {
