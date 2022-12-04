3e4d7d24b8e04c419ee3aa46be8305313a69cdef
Igor Rezende <98923958+Sr-Igor@users.noreply.github.com>
2022-12-04 11:32:45 -0300

    CardsList

---
+++ src/components/CardsList/index.tsx
@@ -0,0 +1,24 @@
+import Heading from 'components/Heading'
+import { PaymentCard } from 'components/PaymentOptions'
+import * as S from './styles'
+
+export type CardListProps = {
+  cards: PaymentCard[]
+}
+
+const CardsList = ({ cards }: CardListProps) => (
+  <>
+    <Heading lineBottom lineColor="secondary" size="small">
+      My Cards
+    </Heading>
+
+    {cards?.map((card) => (
+      <S.Card key={card.number}>
+        <img src={card.img} alt={card.flag} />
+        <span>{card.number}</span>
+      </S.Card>
+    ))}
+  </>
+)
+
+export default CardsList
---
+++ src/components/CardsList/test.tsx
@@ -0,0 +1,26 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+import cardsMock from 'components/PaymentOptions/mock'
+
+import CardsList from '.'
+
+const props = {
+  cards: cardsMock
+}
+
+describe('<CardsList />', () => {
+  it('should render cards list', () => {
+    renderWithTheme(<CardsList {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: /my cards/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('img', { name: /visa/i })).toHaveAttribute(
+      'src',
+      '/img/visa.png'
+    )
+
+    expect(screen.getByText(/4325/)).toBeInTheDocument()
+  })
+})
