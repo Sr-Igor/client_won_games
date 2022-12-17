3b3bffa92f4cdc2b9c9c271e9404efb68c55771b
Igor Rezende <98923958+Sr-Igor@users.noreply.github.com>
2022-12-17 11:58:24 -0300

    Refactor GQL Queries

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,40 @@
 import Home, { HomeTemplateProps } from 'templates/Home'
 
-import { items as bannerMock } from 'components/BannerSlider/mock'
 import { items as gamesMock } from 'components/GameCardSlider/mock'
 import { item as highlightMock } from 'components/Highlight/mock'
+import { initializeApollo } from 'utils/apollo'
+import { QUERY_HOME } from 'graphql/queries/home'
+import { QueryHome } from 'graphql/generated/QueryHome'
 
 export default function Index(props: HomeTemplateProps) {
   return <Home {...props} />
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query<QueryHome>({
+    query: QUERY_HOME,
+    variables: {
+      limit: 15
+    }
+  })
+
+  console.log(data.banners)
+
   return {
     props: {
-      banners: bannerMock,
+      revalidate: 60,
+      banners: data.banners.map((banner) => ({
+        img: `http://localhost:1337${banner.image?.url}`,
+        title: banner.title,
+        subtitle: banner.subtitle,
+        buttonLabel: banner.button?.label ?? '',
+        buttonLink: banner.button?.links ?? '',
+        ribbon: banner.ribbon?.text ?? '',
+        ribbonColor: banner.ribbon?.color ?? '',
+        ribbonSize: banner.ribbon?.sizes ?? ''
+      })),
       newGames: gamesMock,
       mostPopularHighlight: highlightMock,
       mostPopularGames: gamesMock,
