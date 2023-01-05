2b1fe5ccf27f215abba7d49271564a932fd8c621
Igor Rezende <98923958+Sr-Igor@users.noreply.github.com>
2023-01-04 22:01:00 -0300

    Joi validation for sigin and sigup

---
+++ src/utils/validation/index.ts
@@ -0,0 +1,52 @@
+import { UsersPermissionsRegisterInput } from 'graphql/generated/globalTypes'
+import Joi from 'joi'
+
+const fieldsValidation = {
+  email: Joi.string()
+    .email({ tlds: { allow: false } })
+    .required(),
+  password: Joi.string().required(),
+  username: Joi.string().min(3).required(),
+  confirm_password: Joi.string()
+    .valid(Joi.ref('password'))
+    .required()
+    .messages({
+      'any.only': 'confirm password must be equal to password'
+    })
+}
+
+export type FieldsErrors = {
+  [key: string]: string
+}
+
+const getFieldsErrors = (error: Joi.ValidationResult) => {
+  const errors: FieldsErrors = {}
+
+  if (error.error) {
+    error.error.details.forEach((err) => {
+      errors[err.path.join('.') as string] = err.message
+    })
+  }
+
+  return errors
+}
+
+export const signUpValidate = (data: UsersPermissionsRegisterInput) => {
+  const { email, password, username, confirm_password } = fieldsValidation
+  const schema = Joi.object({
+    email,
+    password,
+    username,
+    confirm_password
+  })
+
+  return getFieldsErrors(schema.validate(data, { abortEarly: false }))
+}
+
+type SignInData = Omit<UsersPermissionsRegisterInput, 'username'>
+export const signInValidate = (data: SignInData) => {
+  const { email, password } = fieldsValidation
+  const schema = Joi.object({ email, password })
+
+  return getFieldsErrors(schema.validate(data, { abortEarly: false }))
+}
